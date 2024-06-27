05e46cf1d381d95c4a5d6748d2a07ca815479d8b
Mariella Lorenzo Suarez <marye94.lorenzo@gmail.com>
2024-06-27 03:28:44 +0000

    set up template engine

--- server.js
+++ server.js
@@ -6,14 +6,20 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
 fccTesting(app); //For FCC testing purposes
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+  res.render('index');
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
